2dd62f00df1d149ae25644da3b3b7c90282ca0f6
Christian Jöcker <christian.joecker@maibornwolff.de>
2022-12-10 22:09:25 +0100

    fix skills styles

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -4,6 +4,25 @@ import { SkillsType } from '../../constants/skills';
 import { useWindowSize } from '../../hooks/useWindowSize';
 
 import { Balls } from './balls';
+import { TransparentBox } from '../shared/TransparentBox';
+
+const HeaderItem = ({
+  children,
+  isMiddle,
+  color,
+}: {
+  children: string;
+  isMiddle?: boolean;
+  color?: string;
+}) => (
+  <div
+    className={`flex items-center justify-center h-full select-none ${
+      isMiddle ? 'flex-grow-0' : 'flex-1'
+    } ${color}`}
+  >
+    {children}
+  </div>
+);
 
 interface Props {
   skills: SkillsType[];
@@ -11,23 +30,18 @@ interface Props {
 
 export function Skills({ skills }: Props) {
   const { isMobile } = useWindowSize();
-  const HeaderItem = ({ children, isMiddle }: { children: string, isMiddle?:boolean }) => (
-    <div className={`flex items-center justify-center h-full select-none ${isMiddle? 'flex-1' : 'flex-grow-0'}`}>
-      {children}
-    </div>
-  );
   return (
-    <div className={`relative w-full ${isMobile ? 'w-96' : 'w-72'}`}>
+    <TransparentBox className={`relative w-[500px] ${isMobile ? 'h-[450px]' : 'h-[400px]'}`}>
       <div className="absolute top-0 left-0 w-full pointer-events-none">
-        <h4 className="m-2 flex h-24">
-          <HeaderItem>Frontend</HeaderItem>
+        <h4 className="my-6 mx-2 flex text-xl font-light text-center">
+          <HeaderItem color="text-sky-400">Frontend</HeaderItem>
           <HeaderItem isMiddle>+</HeaderItem>
-          <HeaderItem>UX/UI Design</HeaderItem>
+          <HeaderItem color="text-yellow-400">UX/UI Design</HeaderItem>
         </h4>
       </div>
-      <div className="absolute top-0 left-0 w-full h-full">
+      <div className="absolute top-0 left-0 w-full h-full p-2">
         <Balls skills={skills} />
       </div>
-    </div>
+    </TransparentBox>
   );
 }
