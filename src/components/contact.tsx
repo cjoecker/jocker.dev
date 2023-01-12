4b97adb68195ef120b78a16e7b38223974903c73
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-12 21:11:14 +0100

    refactor contact buttons

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,13 +4,16 @@ export const Contact = () => {
   const icons = require.context('../images/', false);
   return (
     <div className="w-full flex flex-col mt-48">
-      <h2 className="text-primary text-4xl font-normal text-left">
-        Contact me!
-      </h2>
-      <div className="flex gap-6 mt-6 mx-auto">
+      <h2 className="text-primary text-4xl font-normal">Contact me!</h2>
+      <div className="flex gap-12 mt-8 mx-auto mb-8">
         {ContactInformation.map(info => {
           return (
-            <div className="p-4 bg-tag rounded-full w-12 h-12 flex cursor-pointer">
+            <button
+              onClick={() => {
+                window.location.href = info.href;
+              }}
+              className=" bg-tag rounded-full w-16 h-16 flex cursor-pointer"
+            >
               <img
                 width="84"
                 height="84"
@@ -18,7 +21,7 @@ export const Contact = () => {
                 src={icons(`./${info.icon}.svg`)}
                 className="m-auto w-full h-full"
               />
-            </div>
+            </button>
           );
         })}
       </div>
