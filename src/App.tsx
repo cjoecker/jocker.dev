1521844a0363de8aee8991595dbd178610f4749a
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-12 16:11:29 +0100

    add facts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Facts } from './components/facts';
 import { Title } from './components/title';
 
 export function App() {
-  return <Title />;
+  return (
+    <div className="text-base">
+      <Title />
+      <Facts />
+    </div>
+  );
 }
---
+++ src/components/facts.tsx
@@ -0,0 +1,35 @@
+export const Facts = () => {
+  return (
+    <div className="h-screen w-full flex flex-col">
+      <div className="my-24 text-xl">
+        <p>
+          I’m passionate about creating{' '}
+          <TextHighlight>great experiences</TextHighlight> with{' '}
+          <TextHighlight>beautiful web applications</TextHighlight>.
+        </p>
+        <p className="mt-4">
+          <TextHighlight>Happy customers</TextHighlight>,{' '}
+          <TextHighlight>clean code</TextHighlight> and{' '}
+          <TextHighlight>sustainable architectures</TextHighlight> are my
+          priority.
+        </p>
+      </div>
+      <div className="flex gap-8 justify-around mx-16">
+        <Fact number={8} label={"Years of experience"}/>
+        <Fact number={23} label={"Developed apps"}/>
+        <Fact number={13} label={"Happy customers"}/>
+      </div>
+    </div>
+  );
+};
+
+const TextHighlight = ({ children }: { children: React.ReactNode }) => (
+  <span className="text-primary font-medium">{children}</span>
+);
+
+const Fact = ({ number, label }: { number: number; label: string }) => (
+  <div className="bg-fact flex flex-col min-w-[200px] p-4 rounded-lg">
+    <div className="text-9xl text-primary">{number}+</div>
+    <div className="text-2xl mt-2">{label}</div>
+  </div>
+);
