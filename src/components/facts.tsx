684b86f33a3a552fdb495b6a14b733e143c5eddb
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-15 10:36:04 +0100

    add facts animation

--- src/components/facts.tsx
+++ src/components/facts.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from 'react';
+import { animate } from 'framer-motion';
+
 export const Facts = () => {
   return (
     <div className="w-full flex flex-col">
@@ -28,8 +31,30 @@ const TextHighlight = ({ children }: { children: React.ReactNode }) => (
 );
 
 const Fact = ({ number, label }: { number: number; label: string }) => (
-  <div className="bg-fact flex flex-col min-w-[200px] p-4 rounded-lg">
-    <div className="text-9xl text-primary">{number}+</div>
+  <div className="bg-fact flex flex-col min-w-[220px] p-4 rounded-lg">
+    <Counter number={number} />
     <div className="text-2xl mt-2">{label}</div>
   </div>
 );
+
+function Counter({ number }: { number: number }) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const controls = animate(0, number + 1, {
+      duration: number / 8,
+      onUpdate(value) {
+        if (!ref.current) {
+          return;
+        }
+        if (value >= number) {
+          ref.current.textContent = `${number}+`;
+        } else {
+          ref.current.textContent = value.toFixed(0);
+        }
+      },
+    });
+    return () => controls.stop();
+  }, [number]);
+
+  return <div ref={ref} className="text-9xl text-primary" />;
+}
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,11 +1,25 @@
+import { motion } from 'framer-motion';
+
 export const Title = () => {
   return (
     <div className="h-screen w-full flex">
       <div className="m-auto">
-        <h1 className="text-6xl font-normal text-primary">Christian Jöcker</h1>
-        <h2 className="font-normal mt-1">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="text-6xl font-normal text-primary"
+        >
+          Christian Jöcker
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3, delay: 0.5 }}
+          className="font-normal mt-1"
+        >
           Frontend Developer & UX/UI Designer
-        </h2>
+        </motion.h2>
       </div>
     </div>
   );
