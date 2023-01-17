ee65eca44f03c08d96ebe92c05421cf6d9f87e16
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-17 18:23:57 +0100

    fix buttons animation

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -18,27 +18,30 @@ export const Contact = () => {
       >
         {ContactInformation.map(info => {
           return (
-            <motion.button
+            <motion.div
               key={info.text}
               variants={{
                 visible: { scale: 1 },
                 hidden: { scale: 0 },
               }}
-              whileTap={{ scale: 1 }}
-              whileHover={{ scale: 1.2 }}
-              onClick={() => {
-                window.location.href = info.href;
-              }}
-              className=" bg-tag rounded-full w-16 h-16 flex cursor-pointer p-3"
             >
-              <img
-                width="84"
-                height="84"
-                alt={info.text}
-                src={icons(`./${info.icon}.svg`)}
-                className="m-auto w-full h-full"
-              />
-            </motion.button>
+              <motion.button
+                whileTap={{ scale: 1 }}
+                whileHover={{ scale: 1.2 }}
+                onClick={() => {
+                  window.location.href = info.href;
+                }}
+                className=" bg-tag rounded-full w-16 h-16 flex cursor-pointer p-3"
+              >
+                <img
+                  width="84"
+                  height="84"
+                  alt={info.text}
+                  src={icons(`./${info.icon}.svg`)}
+                  className="m-auto w-full h-full"
+                />
+              </motion.button>
+            </motion.div>
           );
         })}
       </motion.div>
