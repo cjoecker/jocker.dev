a33c45d745199b5d708a4b8cd11bbad6ad1c7e3e
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-18 15:51:44 +0100

    refactor core values

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -56,8 +56,8 @@ export const ContactInformation: ContactInformationType[] = [
 export const CoreValuesData: CoreValuesType[] = [
   {coreValue: 'Excellence', explanation:'Sustainable architecture, clean code, and great UX are my mantras.'},
   {coreValue: 'Accountable', explanation:'I stand for my decisions, achievements and mistakes.'},
-  {coreValue: 'Passion', explanation:'I love with I do and you will notice it!'},
-  {coreValue: 'Kindness', explanation:'Always eager to help and understand the other side'}
+  {coreValue: 'Passion', explanation:'I love what I do and you will notice it!'},
+  {coreValue: 'Kindness', explanation:'Always eager to help and understand the other side.'}
 ]
 
 export const Courses: CourseType[] = [
