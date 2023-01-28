678d27bef7cfe453181a2f423b1ebc119a94ad66
Christian Jöcker <christian.joecker@maibornwolff.de>
2023-01-29 00:34:28 +0100

    add testimonials

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -1,137 +1,139 @@
-import { testimonials, TestimonialsType } from '../../constants/testimonials';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { testimonials } from '../../constants/testimonials';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Autoplay, EffectCoverflow } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+
+import { Section } from '../shared/section';
+import { getAltTextFromFileName } from '../shared/utils';
+import { useNarrowView } from '../../hooks/useNarrowView';
 
 export function Testimonials() {
-  const [[activeIndex, direction], setActiveIndex] = useState([0, 0]);
-  const indexInArrayScope =
-    ((activeIndex % testimonials.length) + testimonials.length) %
-    testimonials.length;
-  const visibleTestimonials = [...testimonials, ...testimonials].slice(
-    indexInArrayScope,
-    indexInArrayScope + 3
-  );
-  const handleClick = (newDirection: Variants['direction']) => {
-    setActiveIndex(prevIndex => [prevIndex[0] + newDirection, newDirection]);
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { isNarrowView } = useNarrowView();
+  useEffect(() => {
+    console.log('currentIndex', currentIndex);
+  }, [currentIndex]);
 
   return (
-    <div className="main-wrapper">
-      <div className="flex">
-        <AnimatePresence mode="popLayout" initial={false}>
-          {visibleTestimonials.map(testimonial => {
+    <Section title="Testimonials">
+      <Swiper
+        className="mask-swiper-narrow md:mask-swiper"
+        onSlideChange={swiper =>
+          setCurrentIndex(swiper.activeIndex % testimonials.length)
+        }
+        effect={'coverflow'}
+        centeredSlides
+        grabCursor
+        shortSwipes={false}
+        slidesPerView={isNarrowView ? 1.1 : 2.2}
+        slideToClickedSlide
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: true,
+        }}
+        loop
+        coverflowEffect={{
+          rotate: 0,
+          stretch: isNarrowView ? 100 : 0,
+          depth: 100,
+          scale: 0.7,
+          slideShadows: false,
+        }}
+        modules={[EffectCoverflow, Autoplay]}
+      >
+        {testimonials.map(testimonial => {
+          const images = require.context('../../images/', false);
+          return (
+            <SwiperSlide key={testimonial.testimonial}>
+              {({ isActive }) => (
+                <div
+                  style={{ filter: isActive ? '' : 'blur(1px)' }}
+                  className={`flex-1 select-none flex bg-light-grey shadow-md p-5 flex-col rounded-xl ${
+                    isActive ? '' : 'opacity-70'
+                  }`}
+                >
+                  <img
+                    width="150"
+                    height={testimonial.companyHeight}
+                    className="mx-auto object-contain mt-2"
+                    alt={getAltTextFromFileName(testimonial.companyLogo)}
+                    src={images(`./${testimonial.companyLogo}`)}
+                  />
+                  <img
+                    width="20"
+                    height="20"
+                    className="ml-2 mb-2 mt-4"
+                    alt="double quotes"
+                    src={images(`./double-quotes.svg`)}
+                  />
+                  <div>{testimonial?.testimonial}</div>
+                  <div className="flex mx-auto justify-end text-left mt-12">
+                    <img
+                      width="80"
+                      height="80"
+                      className="my-auto"
+                      alt={getAltTextFromFileName(testimonial?.photo)}
+                      src={images(`./${testimonial?.photo}`)}
+                    />
+                    <div className="flex flex-col justify-end my-auto ">
+                      <div className="text-lg font-bold">
+                        {testimonial?.person}
+                      </div>
+                      <div>{testimonial?.title}</div>
+                      <div className="text-sm opacity-80">
+                        {testimonial?.company}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </SwiperSlide>
+          );
+        })}
+        <div className="flex justify-center mt-0 md:mt-6">
+          {testimonials.map((_, index) => {
             return (
-              <motion.div
-                className="card"
-                key={testimonial.testimonial}
-                layout
-                custom={{
-                  direction,
-                  position: () => {
-                    if (
-                      testimonial.testimonial ===
-                      visibleTestimonials[0].testimonial
-                    ) {
-                      return 'left';
-                    } else if (
-                      testimonial.testimonial ===
-                      visibleTestimonials[1].testimonial
-                    ) {
-                      return 'center';
-                    } else {
-                      return 'right';
-                    }
-                  },
-                }}
-                variants={variants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{ duration: 1 }}
-              >
-                {<Testimonial testimonial={testimonial} />}
-              </motion.div>
+              <PaginationButtons
+                key={index}
+                index={index}
+                isSelected={currentIndex === index}
+              />
             );
           })}
-        </AnimatePresence>
-      </div>
-      <div className="buttons">
-        <motion.button
-          whileTap={{ scale: 0.8 }}
-          onClick={() => handleClick(-1)}
-        >
-          ◀︎
-        </motion.button>
-        <motion.button whileTap={{ scale: 0.8 }} onClick={() => handleClick(1)}>
-          ▶︎
-        </motion.button>
-      </div>
-    </div>
+        </div>
+      </Swiper>
+    </Section>
   );
-}
-
-type Variants = {
-  direction: -1 | 1;
-  position: () => 'center' | 'left' | 'right';
-};
-
-const variants = {
-  enter: ({ direction }: Variants) => {
-    return { scale: 0.2, x: direction < 1 ? 50 : -50, opacity: 0 };
-  },
-  center: ({ position, direction }: Variants) => {
-    return {
-      scale: position() === 'center' ? 1 : 0.7,
-      x: 0,
-      zIndex: getZIndex({position,direction}),
-      opacity: position() === 'center' ? 1 : 0.5,
-    };
-  },
-  exit: ({ direction }: Variants) => {
-    return { scale: 0.2, x: direction < 1 ? -50 : 50, opacity: 0 };
-  },
-};
-
-function getZIndex({ position, direction }: Variants){
-  const indexes = {
-    left:  direction > 0 ? 2 : 1,
-    center: 3,
-    right: direction > 0 ? 1 : 2,
-  };
-  return indexes[position()]
 }
 
 export type Props = {
-  testimonial: TestimonialsType;
+  index: number;
+  isSelected: boolean;
 };
-export const Testimonial = ({ testimonial }: Props) => {
-  const images = require.context('../../images/', false);
+export const PaginationButtons = ({ index, isSelected }: Props) => {
+  const swiper = useSwiper();
   return (
-    <div className="flex-1 flex bg-light-grey shadow-md p-5 flex-col rounded-xl w-[200px]">
-      <div>
-        {/*<img*/}
-        {/*  width="100"*/}
-        {/*  height="100"*/}
-        {/*  className=""*/}
-        {/*  alt={getAltTextFromFileName(testimonial.companyLogo)}*/}
-        {/*  src={images(`./${testimonial.companyLogo}`)}*/}
-        {/*/>*/}
-      </div>
-      <span className="text-primary text-8xl text-left -mb-14">“</span>
-      <div>{testimonial?.testimonial}</div>
-      <div className="flex mx-auto mt-8 ml-auto">
-        <div className="flex flex-col justify-end text-left my-auto">
-          <div className="text-lg font-bold">{testimonial?.person}</div>
-          <div>{testimonial?.title}</div>
-        </div>
-        {/*<img*/}
-        {/*  width="100"*/}
-        {/*  height="100"*/}
-        {/*  className=""*/}
-        {/*  alt={getAltTextFromFileName(testimonial?.photo)}*/}
-        {/*  src={images(`./${testimonial?.photo}`)}*/}
-        {/*/>*/}
-      </div>
-    </div>
+    <motion.button
+      whileHover={{ scale: 1.3 }}
+      whileTap={{ scale: 1 }}
+      className="hover:cursor-pointer p-8 md:p-2"
+      onClick={() => {
+        swiper.slideTo(index);
+        console.log("index", index);
+      }}
+    >
+      <div
+        style={{
+          background: isSelected
+            ? 'linear-gradient(145deg, #0C4E80, #062e4d)'
+            : 'linear-gradient(145deg, #778088, #4b5157)',
+        }}
+        className={`w-2 h-2 bg-primary rounded-full bg-pagination bg-pagination-blue`}
+      />
+    </motion.button>
   );
 };
