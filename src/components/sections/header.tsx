bd65a429ec2cd4d928252f1b590be71ef7915282
Christian Jöcker <c.jocker@hotmail.com>
2023-01-31 22:33:22 +0100

    redesign header

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -4,7 +4,7 @@ import LightBulb from '../../images/light-bulb.svg';
 export const Header = () => {
   return (
     <div className="h-screen w-full flex items-end relative">
-      <div className="ml-24 mb-24 text-left ">
+      <div className="ml-24 mb-24 text-left">
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -22,17 +22,20 @@ export const Header = () => {
           Freelance Frontend Developer
           <br />& UX/UI Designer
         </motion.h2>
-        <button className="bg-button-header rounded-full py-2 px-4 text-xl mt-6 text-light-grey">
+        <button className="bg-button-header rounded-full py-2 px-4 text-xl mt-12 text-light-grey">
           Discover More
         </button>
       </div>
-      <img
-        src={LightBulb}
-        alt="light bulb shining"
-        width="200"
-        height="200"
-        className="bg-light-bulb"
-      />
+      <div className="overflow-hidden absolute w-full h-full top-0 left-0 flex justify-end">
+        <img
+          className="translate-x-1/2"
+          src={LightBulb}
+          alt="light bulb shining"
+          width="500"
+          height="100%"
+        />
+      </div>
+
     </div>
   );
 };
