ddbb0d56424bdcb0eff9905f266dae76abacf131
Christian Jöcker <c.jocker@hotmail.com>
2023-02-11 10:00:14 +0100

    redesign header button

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -24,6 +24,9 @@ export const Header = () => {
   const subtitleY = useParallax(scrollY, -0.5);
   const buttonY = useParallax(scrollY, -1);
 
+  // there is a bug in chromium that is not showing -webkit-fill-available correctly
+  const isIphone = window.navigator.userAgent.includes('iPhone');
+
   const { elX, elY } = useMouse(ref);
   const mouseX = useMotionValue(0);
   const mouseY = useMotionValue(0);
@@ -47,44 +50,43 @@ export const Header = () => {
         mouseX.set(elX / 8);
         mouseY.set(elY / 8);
       }}
-      className={`w-full flex items-end relative overflow-x-hidden h-screen`}
+      className={`w-full flex items-end relative overflow-x-hidden h-screen ${
+        isIphone ? 'h-ios-screen' : 'h-screen'
+      }`}
       ref={ref}
     >
       <Background mouseX={mouseX} mouseY={mouseY} />
       <div
         className={`${
-          isNarrowView ? 'mx-12 mb-12' : 'ml-24 mb-20'
+          isNarrowView ? 'mx-12 mb-12' : 'ml-24 mb-28'
         } text-left z-10`}
       >
         <motion.h1 style={{ y: titleY }} className="text-2xl font-bold">
           Hi, I'm Christian Jöcker,
-          <br />a full stack developer.
+          <br />a Full-Stack Developer.
         </motion.h1>
         <motion.p
           style={{ y: subtitleY }}
-          className="font-normal text-xl mt-12 max-w-[70vw]"
+          className="font-normal text-lg mt-8 mr-[25vw]"
         >
           I work as a freelance developer and am passionate about creating great
           experiences with beautiful web applications!
         </motion.p>
         <motion.button
-          style={{ y: buttonY }}
+          style={{ y: buttonY, boxShadow: '0px 0px 100px 0px #00DFD866' }}
           whileTap={{ scale: 1 }}
-          whileHover={{ scale: 1.2 }}
+          whileHover={{ scale: 1.1 }}
           onClick={onDiscoverMoreClick}
-          className="rounded-full font-semibold py-5 px-8 text-lg hover:cursor-pointer select-none text-secondary bg-gradient-to-br from-primary to-primary/50 mb-[10vh] mt-[15vh]"
+          className="rounded-md font-semibold text-lg hover:cursor-pointer select-none text-secondary bg-gradient-to-br from-turquoise to-blue mt-24"
         >
-          Discover More
+          <div className="py-4 px-6 m-[1px] bg-secondary rounded-md bg-[#000] bg-opacity-80">
+            Discover More
+          </div>
         </motion.button>
       </div>
     </div>
   );
 };
-
-function useParallax(scrollY: MotionValue<number>, multiplicator: number) {
-  return useTransform(scrollY, value => value * multiplicator);
-}
-
 export type Props = {
   mouseX: MotionValue<number>;
   mouseY: MotionValue<number>;
@@ -95,19 +97,25 @@ export const Background = ({ mouseX, mouseY }: Props) => {
   return (
     <>
       <motion.img
+        aria-hidden="true"
         style={{ x: mouseX, y: mouseY }}
         className="absolute right-[-50vw] top-[-20vh] w-[130vw] h-[85vh]"
         src={MeshPurple}
       />
       <motion.img
+        aria-hidden="true"
         style={{ x: blueMeshX, y: blueMeshY }}
         className="absolute right-[-45vw] top-[5vh] w-[120vw] h-[120vh]"
         src={MeshTurquoise}
       />
       <motion.img
+        aria-hidden="true"
         className="absolute left-[-45vw] top-[-15vh] w-[120vw] h-[120vh]"
         src={MeshBlue}
       />
     </>
   );
 };
+function useParallax(scrollY: MotionValue<number>, multiplicator: number) {
+  return useTransform(scrollY, value => value * multiplicator);
+}
