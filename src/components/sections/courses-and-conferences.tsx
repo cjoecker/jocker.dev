a77e713bc23d3375bda6c2e2a2fcfc96eb3b34d9
Christian Jöcker <c.jocker@hotmail.com>
2023-02-22 18:04:11 +0100

    fix max widths

--- src/components/sections/courses-and-conferences.tsx
+++ src/components/sections/courses-and-conferences.tsx
@@ -25,7 +25,7 @@ export const CoursesAndConferences = () => {
   return (
     <Section title="Last Attended Courses and Conferences">
       <div className="w-full flex flex-col mx-8">
-        <div className="flex relative">
+        <div className="flex relative mx-auto max-w-7xl">
           <img
             aria-hidden="true"
             className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10 w-[250%] h-[250%]"
