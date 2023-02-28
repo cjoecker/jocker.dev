efb4e376b6f62803a95389d4369541901bf416ed
Christian Jöcker <c.jocker@hotmail.com>
2023-02-28 20:47:07 +0100

    add stagger animations to service offer

--- app/hooks/useNarrowView.ts
+++ app/hooks/useNarrowView.ts
@@ -1,8 +1,19 @@
-import { useWindowSize } from 'react-use';
+import { useLayoutEffect, useMemo, useState } from 'react';
 
 const NARROW_WIDTH = 480;
 export function useNarrowView() {
-	const { width } = useWindowSize();
-	const isNarrowView = width <= NARROW_WIDTH;
+	const [width, setWidth] = useState(
+		typeof window !== 'undefined' ? window.innerWidth : 0
+	);
+	useLayoutEffect(() => {
+		function handleResize() {
+			if (typeof window !== 'undefined') {
+				setWidth(window.innerWidth);
+			}
+		}
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+	const isNarrowView = useMemo(() => width <= NARROW_WIDTH, [width]);
 	return { isNarrowView };
 }
