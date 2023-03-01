1778af1883da83f64e588aee4fc4b7a088e77f9b
Christian Jöcker <c.jocker@hotmail.com>
2023-03-01 20:12:41 +0100

    fix links

---
+++ app/components/shared/external-redirect.tsx
@@ -0,0 +1,26 @@
+export const ExternalRedirect = ({
+	children,
+	className,
+	to,
+}: {
+	children: React.ReactNode;
+	className?: string;
+	to: string;
+}) => {
+	return (
+		<a
+			href={to}
+			target="_blank"
+			onClick={e => {
+				(e.target as HTMLLinkElement).blur();
+				e.stopPropagation()
+			}}
+			className={className}
+			rel="noreferrer"
+		>
+			<div className="pointer-events-none">
+				{children}
+			</div>
+		</a>
+	);
+};
