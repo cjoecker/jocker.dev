41d6f49745e227032dd1bacf9fbd818762d19dc6
Christian Jöcker <c.jocker@hotmail.com>
2023-11-12 20:31:39 +0100

    update dependencies

--- remix.config.js
+++ remix.config.js
@@ -9,12 +9,4 @@ module.exports = {
 			: undefined,
 	serverBuildPath: '.netlify/functions-internal/server.js',
 	serverModuleFormat: 'cjs',
-	future: {
-		v2_routeConvention: true,
-		v2_errorBoundary: true,
-		v2_normalizeFormMethod: true,
-		v2_meta: true,
-		v2_headers: true,
-		v2_dev: true,
-	},
 };
