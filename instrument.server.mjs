a3a895a6a33320f71e8b9701e44081ddbe0fff4d
Christian Jöcker <c.jocker@hotmail.com>
2025-05-08 10:15:52 +0200

    remove instrument.server.mjs

--- instrument.server.mjs
+++
@@ -1,6 +0,0 @@
-import * as Sentry from "@sentry/react-router";
-
-Sentry.init({
-	dsn: "https://5d92d09a957dedb8e6084f2777b792af@o4509241568722944.ingest.de.sentry.io/4509241895616592",
-	sendDefaultPii: true,
-});
