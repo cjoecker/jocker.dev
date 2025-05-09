401ce6a6716f719951c18415f1e81b1e41c11490
Christian Jöcker <c.jocker@hotmail.com>
2025-05-09 13:39:09 +0200

    add general fixes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,10 @@
-import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
-import { ActionFunctionArgs, useActionData } from "react-router";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { useLocation } from "react-use";
 
 import { AboutMe } from "~/components/sections/about-me";
-import { Contact, ContactFormAlert } from "~/components/sections/contact";
+import { Contact } from "~/components/sections/contact";
 import { Contributions } from "~/components/sections/contributions";
 import { CoreValues } from "~/components/sections/core-values";
 import { CoursesAndConferences } from "~/components/sections/courses-and-conferences";
@@ -15,72 +16,92 @@ import { Languages } from "~/components/sections/languages";
 import { ServiceOffer } from "~/components/sections/service-offer";
 import { Skills } from "~/components/sections/skills";
 import { Testimonials } from "~/components/sections/testimonials";
-
-const ALERT_DURATION = 5000;
-
-export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData();
-	const name = formData.get("name") as string;
-	const email = formData.get("email") as string;
-	const message = formData.get("message") as string;
-
-	const baseUrl = request.url;
-
-	try {
-		await fetch(`${baseUrl}/form`, {
-			method: "POST",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: `name=${name}&email=${email}&message=${message}&form-name=contact`,
-		});
-	} catch (error) {
-		console.error("Error sending form data:", error);
-		return { success: false, personalEmail: process.env.PERSONAL_EMAIL };
-	}
-
-	return { success: true };
-}
+import { Button, ButtonIcon } from "~/components/shared/button";
+import { ContactInformation } from "~/constants/contact-information";
+import useVisibleSection from "~/hooks/use-visible-section";
 
 export default function Index() {
-	const data = useActionData<typeof action>();
-	const [isContactFormAlertVisible, setIsContactFormAlertVisible] =
-		useState(false);
-	useEffect(() => {
-		if (data && "success" in data) {
-			setIsContactFormAlertVisible(true);
-			if (data.success) {
-				setTimeout(() => {
-					setIsContactFormAlertVisible(false);
-				}, ALERT_DURATION);
-			}
-		}
-	}, [data]);
 	return (
 		<main className="overflow-x-hidden text-base font-normal">
 			<Header />
 			<div className="flex w-full flex-col">
 				<div className="">
 					<ServiceOffer />
-					<Facts />
-					<Skills />
 					<CoreValues />
-					<ExperienceAndEducation />
-					<Languages />
+					<Skills />
+					<Facts />
 					<Testimonials />
+					<ExperienceAndEducation />
 					<Contributions />
 					<CoursesAndConferences />
+					<Languages />
 					<AboutMe />
 					<Contact />
 					<Footer />
 				</div>
 			</div>
-			<AnimatePresence>
-				{isContactFormAlertVisible && (
-					<ContactFormAlert
-						type={data?.success ? "success" : "error"}
-						personalEmail={data?.personalEmail ?? ""}
-					/>
-				)}
-			</AnimatePresence>
+			<Outlet />
+			<MessageFloatingButton />
 		</main>
 	);
 }
+
+// empty string is the header section
+const hiddenMessageButtonSections = new Set([
+	"",
+	"what_i_can_do_for_you",
+	"contact_me!",
+]);
+
+export const MessageFloatingButton = () => {
+	const { visibleSection } = useVisibleSection();
+	const [isVisible, setIsVisible] = useState(false);
+	const navigate = useNavigate();
+	const contactInformation = ContactInformation.find((contact) => {
+		return contact.alt === "message";
+	});
+	// get current path from remix
+	const location = useLocation();
+
+	useEffect(() => {
+		if (
+			hiddenMessageButtonSections.has(visibleSection) ||
+			location.pathname === "/contact"
+		) {
+			setIsVisible(false);
+		} else {
+			setIsVisible(true);
+		}
+	}, [visibleSection, location]);
+
+	if (!contactInformation) {
+		return null;
+	}
+
+	return (
+		<AnimatePresence>
+			{isVisible && (
+				<motion.div
+					className="fixed right-2 bottom-2 z-50"
+					initial={{ y: 100, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					exit={{ y: 100, opacity: 0 }}
+					transition={{ type: "spring", bounce: 0.65, duration: 1.2 }}
+				>
+					<Button
+						iconButton
+						ariaLabel={contactInformation.text}
+						onClick={() => {
+							void navigate("/contact", { preventScrollReset: true });
+						}}
+					>
+						<ButtonIcon
+							alt={contactInformation.alt}
+							src={contactInformation.image}
+						/>
+					</Button>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	);
+};
