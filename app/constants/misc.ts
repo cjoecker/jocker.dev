749005b6a7569772966e3089144930e73b53748a
Christian Jöcker <c.jocker@hotmail.com>
2025-05-09 16:11:30 +0200

    remove trailing slashes

--- app/constants/misc.ts
+++ app/constants/misc.ts
@@ -11,9 +11,9 @@ export const sentryConfig: BrowserOptions | NodeOptions = {
 
 export function getDomainUrl(request: Request) {
 	const host =
-		request.headers.get('X-Forwarded-Host') ??
-		request.headers.get('host') ??
+		request.headers.get("X-Forwarded-Host") ??
+		request.headers.get("host") ??
 		new URL(request.url).host
-	const protocol = request.headers.get('X-Forwarded-Proto') ?? 'http'
+	const protocol = request.headers.get("X-Forwarded-Proto") ?? "http"
 	return `${protocol}://${host}`
 }
